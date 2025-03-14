a5cfdd8bd92cb103c7a2e241bcb0aacffd76861b
tekofx <tekofxx@gmail.com>
2025-03-14 21:27:26 +0100

    Added icon to tea buttons

--- composeApp/src/commonMain/kotlin/dev/tekofx/teatime/components/TeaButton.kt
+++ composeApp/src/commonMain/kotlin/dev/tekofx/teatime/components/TeaButton.kt
@@ -2,16 +2,23 @@ package dev.tekofx.teatime.components
 
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.wrapContentWidth
 import androidx.compose.material.Button
 import androidx.compose.material.ButtonDefaults
 import androidx.compose.material.Text
+import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
 import com.kdroid.composenotification.builder.ExperimentalNotificationsApi
 import dev.tekofx.teatime.model.Tea
 import org.jetbrains.compose.resources.ExperimentalResourceApi
+import org.jetbrains.compose.resources.painterResource
+import teatime.composeapp.generated.resources.Res
+import teatime.composeapp.generated.resources.emoji_food_beverage
+import teatime.composeapp.generated.resources.timer_filled
 
 
 @OptIn(ExperimentalNotificationsApi::class, ExperimentalResourceApi::class)
@@ -22,7 +29,6 @@ fun TeaButton(
     onClick: (Tea)->Unit,
 ) {
     Button(
-        modifier = Modifier.size(100.dp),
         onClick = {
             onClick(tea)
         },
@@ -30,11 +36,16 @@ fun TeaButton(
             backgroundColor = if (activeTea == tea) MaterialTheme.colorScheme.primaryContainer else MaterialTheme.colorScheme.primary
         )
     ) {
-        Column {
+        Column(
+            horizontalAlignment = Alignment.CenterHorizontally,
+        ) {
+            Icon(
+                modifier = Modifier.size(60.dp),
+                painter = painterResource(Res.drawable.emoji_food_beverage),
+                contentDescription = ""
+            )
             Text(tea.name)
-            Text("${tea.temperature} ªC")
-            Text(tea.formattedTime)
-            Text(tea.quantity)
+            Text("${tea.temperature} ª ${tea.formattedTime}")
         }
     }
 }
